31819fd738e17edf53d34d6cb837049ea3884e3a
pepperidze <023mvm@gmail.com>
2020-11-15 21:39:02 +0200

    Update p14: rewrite with tail recursion

--- hw02/p14.erl
+++ hw02/p14.erl
@@ -1,10 +1,14 @@
 -module(p14).
+-import(p05, [reverse/1]).
 -export([duplicate/1]).
 
 % p14:duplicate([a,b,c,c,d]).
 % [a,a,b,b,c,c,c,c,d,d]
 
-duplicate([H|T]) ->
-    [H, H|duplicate(T)];
-duplicate([]) ->
-    [].
+duplicate(L) ->
+    duplicate(L, []).
+
+duplicate([H|T], Acc) ->
+    duplicate(T, [H, H|Acc]);
+duplicate([], Acc) ->
+    reverse(Acc).
